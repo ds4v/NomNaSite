4ee38912c49ca7d5f7ab6411f789afafd97fc21f
Quan Dang <18520339@gm.uit.edu.vn>
2023-04-18 04:22:28 +0700

    Implementation for recognize button

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import cv2
-import json
 import hashlib
 import numpy as np
 import streamlit as st
@@ -7,13 +6,13 @@ import streamlit as st
 from PIL import Image
 from streamlit_drawable_canvas import st_canvas
 from handler.asset import download_assets, load_models, file_uploader
-from handler.bbox import transform_fabric_box, get_patch
+from handler.bbox import transform_fabric_box, order_boxes4nom, get_patch
 from handler.translator import hcmus_translate, hvdic_render
 
 
 def img2str(cv2_image):
     img_bytes = cv2.imencode('.jpg', cv2_image)[1].tobytes()
-    hash_object = hashlib.md5(img_bytes)
+    hash_object = hashlib.sha256(img_bytes)
     hash_str = hash_object.hexdigest()
     print(hash_str)
     return hash_str
@@ -28,11 +27,17 @@ image_name = 'test.jpg'
 
 with col1:
     file_uploader(image_name)
-    mode = st.radio('Box Mode:', ('Drawing', 'Editing'), horizontal=True, label_visibility='collapsed')
+    raw_image = cv2.cvtColor(cv2.imread(image_name), cv2.COLOR_BGR2RGB)
     
     with st.spinner('Detecting bounding boxes containing text...'):
-        raw_image = cv2.cvtColor(cv2.imread(image_name), cv2.COLOR_BGR2RGB)
         boxes = det_model.predict_one_page(raw_image)
+        key = img2str(raw_image)
+        
+        col11, col12 = st.columns([4, 1])
+        with col11:
+            mode = st.radio('Mode', ('Drawing', 'Editing'), horizontal=True, label_visibility='collapsed', key=f'mode_{key}')
+        with col12:
+            reg_clicked = st.button('Extract text', type='primary', use_container_width=True)
         
         # https://github.com/andfanilo/streamlit-drawable-canvas/issues/73
         # width, height = raw_image.shape[1], raw_image.shape[0]
@@ -63,22 +68,24 @@ with col1:
             stroke_color = 'red',
             drawing_mode = 'rect' if mode == 'Drawing' else 'transform',
             initial_drawing = initial_drawing,
-            update_streamlit = False,
-            key = img2str(raw_image)
+            update_streamlit = reg_clicked,
+            key = f'canvas_{key}'
         )
         
         
 with col2:
     json_data = canvas_result.json_data
-    canvas_boxes = json_data['objects'] if json_data and 'objects' in json_data else []
-    
-    with st.spinner('Recognizing text in each bounding box...'):
-        for idx, box in enumerate(canvas_boxes[:1]):
-            box4pts = transform_fabric_box(box)
-            patch = get_patch(raw_image, box4pts)
-            text = reg_model.predict_one_patch(patch)
-            st.markdown(f'''
-                <b>Text {idx + 1:02d}</b>: {text}<br/>
-                [hcmus](https://www.clc.hcmus.edu.vn/?page_id=3039): {hcmus_translate(text)}<br/>
-                [hvdic](https://hvdic.thivien.net/transcript.php#trans): {hvdic_render(text)}<hr style="margin: 0;"/>
-            ''', unsafe_allow_html=True)
+    if json_data and 'objects' in json_data:
+        canvas_boxes = [transform_fabric_box(obj) for obj in json_data['objects']]
+        canvas_boxes = order_boxes4nom(canvas_boxes)
+        
+        with st.spinner('Recognizing text in each bounding box...'):
+            for idx, box in enumerate(canvas_boxes):
+                patch = get_patch(raw_image, box)
+                text = reg_model.predict_one_patch(patch)
+                st.markdown(f'''
+                    <b>Text {idx + 1:02d}</b>: {text}<br/>
+                    [hcmus](https://www.clc.hcmus.edu.vn/?page_id=3039): {hcmus_translate(text)}<br/>
+                    [hvdic](https://hvdic.thivien.net/transcript.php#trans): {hvdic_render(text)}
+                    <hr style="margin: 0;"/>
+                ''', unsafe_allow_html=True)
