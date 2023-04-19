85846c9d9903cdc2f5a53b304643076c449c7cbb
Quan Dang <18520339@gm.uit.edu.vn>
2023-04-19 10:07:37 +0700

    Add css.py for customizing style

---
+++ css.py
@@ -0,0 +1,33 @@
+custom_style = '''
+    <style>
+        .block-container > div > 
+        [data-testid="stVerticalBlock"] > 
+        [data-testid="stHorizontalBlock"] > 
+        [data-testid="column"]:nth-of-type(1) {
+            border: 1px solid rgba(49, 51, 63, 0.2);
+            border-radius: 0.25rem;
+            padding: calc(1em - 1px);
+        }
+        .block-container > div > 
+        [data-testid="stVerticalBlock"] > 
+        [data-testid="stHorizontalBlock"] > 
+        [data-testid="column"]:nth-of-type(2) {
+            height: 80vh;
+            overflow-x: hidden;
+            overflow-y: scroll;
+        }
+        .block-container > div > 
+        [data-testid="stVerticalBlock"] > 
+        [data-testid="stHorizontalBlock"] > 
+        [data-testid="column"]:nth-of-type(2) 
+        div:not([data-testid="stExpander"]):not([data-testid="stImage"]) {
+            width: 100%!important;
+        }
+        button:disabled, button:disabled:hover, button:disabled:active {
+            border-color: transparent!important;
+            color: black!important;
+            cursor: auto!important;
+            padding-left: 0px;
+        }
+    </style>
+'''
